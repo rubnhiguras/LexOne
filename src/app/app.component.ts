c6d029ef93da6fed3334520943fbae15480e2963
rubenhigueras <proyectoshigueras@gmail.com>
2026-09-03 12:47:05 +0200

    Version settings bottom page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,24 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   template: `
     <ion-app>
       <ion-router-outlet></ion-router-outlet>
+
+      <footer class="app-footer">
+        LexOne · v{{ version }}
+      </footer>
     </ion-app>
-  `
+  `,
+  styles: [`
+    .app-footer {
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      text-align: center;
+      padding: 4px;
+      font-size: 11px;
+      color: #888;
+    }
+  `]
 })
-export class AppComponent {}
+export class AppComponent {
+  version = version;
+}
